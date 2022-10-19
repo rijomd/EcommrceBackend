4c4988daf4b16b6f21702b643af8187ff6529469
rijomd <rijomd09@gmail.com>
2022-10-19 17:16:42 +0530

    changed filename wishlist control

--- src/controller/Wishlist/wishlistCtrl.js
+++ src/controller/Wishlist/wishlistCtrl.js
@@ -1,5 +1,5 @@
 "use strict";
-const WishListService = require("../../service/WishListService");
+const WishListService = require("../../service/wishListService");
 const log4js = require("log4js");
 const logger = log4js.getLogger();
 logger.level = "debug";
